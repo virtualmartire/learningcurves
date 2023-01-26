744cdb543e5be4998cb18f5b53657b90663be30a
virtualmartire <boomscippo@gmail.com>
2023-01-26 11:52:25 +0100

    localStorage implemented

--- script.js
+++ script.js
@@ -1,34 +1,85 @@
+function loadExistingData() {
+    /* The function that fires when the application starts. */
+
+    const metrics_names_array = Object.keys(localStorage);
+
+    metrics_names_array.forEach((metric_name) => {
+
+        const metric_dict = JSON.parse(localStorage.getItem(metric_name));
+        const runs_names_array = Object.keys(metric_dict);
+
+        runs_names_array.forEach((run_name) => {
+
+            const metric_data = metric_dict[run_name];
+
+            drawCurve(metric_name, run_name, metric_data);
+
+        });
+
+    });
+
+}
+
 function gotFiles(input) {
+    /* The function that fires when some files are loaded. */
 
     const file_list = Array.from(input.files);
 
     file_list.forEach((file) => {
         const reader = new FileReader();
         reader.addEventListener("load", () => {     // because asynchrony
-            const json_string = reader.result;
-            const json_curves = JSON.parse(json_string);
-            const run_name = Object.keys(json_curves)[0];
-            saveRun(run_name, json_string);
-            visualizeData(json_curves);
+
+            const json_dict = JSON.parse(reader.result);
+            const run_name = Object.keys(json_dict)[0];     // every file has only one run
+            const metrics_names_array = Object.keys(json_dict[run_name]);
+
+            metrics_names_array.forEach((metric_name) => {
+
+                const metric_data = json_dict[run_name][metric_name];
+
+                saveMetricData(metric_name, run_name, metric_data);    // data are stored per-metric and not per-run
+                drawCurve(metric_name, run_name, metric_data);
+
+            })
+
         });
         reader.readAsText(file);
     });
 
 }
 
-function saveRun(run_name, json_string) {
-    localStorage.setItem(run_name, json_string);
+function saveMetricData(metric_name, run_name, metric_data) {
+    // Of course, runs are always updated and never taken back to past results
+
+    var metric_dict = JSON.parse(localStorage.getItem(metric_name)) || {};
+
+    metric_dict[run_name] = metric_data;
+    localStorage.setItem(metric_name, JSON.stringify(metric_dict));
+
 }
 
-function visualizeData(json_curves) {
+function drawCurve(metric_name, run_name, metric_data) {
 
-    const run_name = Object.keys(json_curves)[0];
-    const metric_name = Object.keys(json_curves[run_name])[0];
-    const metric_data = json_curves[run_name][metric_name];
-    var chart = getChartById(metric_name) || addChart(metric_name);
+    var chart = getChartById(metric_name) || addChartHTML(metric_name);
+    const epochs = metric_data.map(pair => pair[0]);
+    const values = metric_data.map(pair => pair[1]);
+    var chart_datasets = chart.data.datasets;
+    
+    // Update the x-axis
+    if (chart.data.labels.slice(-1)[0] < epochs.slice(-1)[0]) {
+        chart.data.labels = epochs;
+    }
 
-    drawCurve(run_name, metric_data, chart);
-    chart.update()
+    // Remove the old dataset
+    removeOldDataset(chart_datasets, run_name);
+
+    // Add the new one
+    chart_datasets.push({label: run_name,
+                        data: values,
+                        fill: false});
+    
+    // Re-render the chart
+    chart.update();
 
 }
 
@@ -36,7 +87,7 @@ function getChartById(chart_id) {
     return Object.values(Chart.instances).filter((c) => c.canvas.id == chart_id).pop()
 }
 
-function addChart(metric_name) {
+function addChartHTML(metric_name) {
 
     const new_graph = document.createElement('canvas');
     const graphs_area = document.getElementById('graphs_area');
@@ -61,29 +112,6 @@ function addChart(metric_name) {
                                         });
     
     return chart;
-
-}
-
-function drawCurve(run_name, metric_data, chart) {
-
-    const epochs = metric_data.map(pair => pair[0]);
-    const values = metric_data.map(pair => pair[1]);
-    var chart_datasets = chart.data.datasets;
-    
-    // Update the x-axis
-    if (chart.data.labels.slice(-1)[0] < epochs.slice(-1)[0]) {
-        console.log("oh");
-        chart.data.labels = epochs;
-    }
-
-    // Remove the old dataset
-    removeOldDataset(chart_datasets, run_name);
-
-    // Add the new one
-    chart_datasets.push({label: run_name,
-                        data: values,
-                        fill: false,
-                        tension: 0.1});
 
 }
 
