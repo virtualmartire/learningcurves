26d0bd3165263b66b9cce0675db0bfae5ee54fba
virtualmartire <boomscippo@gmail.com>
2023-03-26 18:18:23 +0200

    side menu wider

--- frontend/scripts/features.js
+++ frontend/scripts/features.js
@@ -1,6 +1,6 @@
 function openNav() {
     /* Open the side menu */
-    document.getElementById("experiments_area").style.width = "300px";
+    document.getElementById("experiments_area").style.width = "325px";
 }
 
 function closeNav() {
